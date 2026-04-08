d9ddcbb6281ace2b97c41cffaf3dc229af0c362e
pytorch1100 <Pytorch1100@163.com>
2026-04-09 06:14:32 +0800

    增加imu消息订阅者，创建订阅者时使用lambda函数。连接mid360测试，可收到liovx_ros_driver2发布的imu话题数据，并执行自定义的回调函数

--- src/fastlio2/src/lio_node.cpp
+++ src/fastlio2/src/lio_node.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 #include <livox_ros_driver2/msg/custom_msg.hpp>
 
 // 需求，写一个node，订阅mid360发布的激光雷达点云和IMU数据
@@ -7,6 +8,7 @@ struct NodeConfig
 {
     /* data */
     std::string lidar_topic = "/livox/lidar";
+    std::string imu_topic = "/livox/imu";
 };
 
 
@@ -18,22 +20,36 @@ public:
     {
         RCLCPP_INFO(this->get_logger(), "LIONode has been started.");
 
-        m_pcd_sub = create_subscription<livox_ros_driver2::msg::CustomMsg>(
+        m_pcd_sub = this->create_subscription<livox_ros_driver2::msg::CustomMsg>(
             m_node_cfg.lidar_topic,
             10,
             std::bind(&LIONode::lidarCallBack, this, std::placeholders::_1));
-    }
 
+        m_imu_sub = this->create_subscription<sensor_msgs::msg::Imu>(
+            m_node_cfg.imu_topic,
+            10,
+            [this](sensor_msgs::msg::Imu::SharedPtr imu_msg){
+                RCLCPP_INFO(this->get_logger(), "recv mid360 imu msg");
+            }
+        );
+    }
 
     void lidarCallBack(const livox_ros_driver2::msg::CustomMsg::SharedPtr livox_msg)  // 接收到livox的lidar点云数据
     {
         RCLCPP_INFO(this->get_logger(), "recv mid360 lidar pointcloud");
     }
 
+    void imuCallBack(const sensor_msgs::msg::Imu::SharedPtr imu_msg)
+    {
+        RCLCPP_INFO(this->get_logger(), "recv mid360 imu msg");
+    }
+
 private:
     NodeConfig m_node_cfg;
 
     rclcpp::Subscription<livox_ros_driver2::msg::CustomMsg>::SharedPtr m_pcd_sub;
+
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr m_imu_sub;   // 指向imu订阅者的共享智能指针，订阅imu话题数据
 };
 
 int main(int argc, char* argv[])
